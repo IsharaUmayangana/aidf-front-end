454051675d78022862ee32939489183c635284b7
Ishara Umayangana <isharaumayangana27@gmail.com>
2024-04-11 16:44:11 +0530

    first commit

---
+++ src/lib/services/api/jobs.ts
@@ -0,0 +1,52 @@
+import { Job } from "@/types/job";
+
+export const getJobs = async () => {
+    const res = await fetch("http://localhost:8000/jobs", {
+        method: "GET"
+    })
+    const data: Job[] = await res.json()
+    return data;
+}
+
+export const getJobById = async (id: String) => {
+    const token = await window.Clerk.session.getToken();
+    const res = await fetch(`http://localhost:8000/jobs/${id}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    const data: Job = await res.json()
+    return data;
+}
+
+export const createJob = async({
+    title,
+    description,
+    type,
+    location,
+    questions
+}: {
+    title: String;
+    description: String;
+    type: String;
+    location: String;
+    questions: String[]
+}) => {
+    const token = window.Clerk.session.getToken();
+
+    await fetch("http://localhost:8000/jobs",{
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization : `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            title,
+            description,
+            type,
+            location,
+            questions
+        })
+    });
+};
