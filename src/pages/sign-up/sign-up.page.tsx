e9ea97c290b67ce04edd48c39b88ce475dacb8de
Ishara Umayangana <isharaumayangana27@gmail.com>
2024-04-11 20:29:15 +0530

    updated aftersign-in/up routes

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react"
 function SignUpPage() {
   return (
     <div className="min h screen flex items-center justify-center">
-      <SignUp afterSignUpUrl={"/home"} signInUrl="/sign-in" />
+      <SignUp afterSignUpUrl={"/"} signInUrl="/sign-in" />
     </div>
   );
 }
